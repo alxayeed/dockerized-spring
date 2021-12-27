dd2493b1c190fa786df932ad12cab643869a442e
alxayeed <alxayeed@gmail.com>
2021-12-27 11:12:47 +0600

    project initialized

---
+++ src/main/java/com/alxayeed/dockerized/DockerizedApplication.java
@@ -0,0 +1,13 @@
+package com.alxayeed.dockerized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alxayeed/dockerized/DockerizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxayeed.dockerized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
