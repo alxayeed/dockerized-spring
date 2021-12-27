96eed28412497443ba905c34bd8c75ebadade0d1
alxayeed <alxayeed@gmail.com>
2021-12-27 15:19:58 +0600

    logger added to main app. db configuration updated

--- src/main/java/com/alxayeed/dockerized/DockerizedApplication.java
+++ src/main/java/com/alxayeed/dockerized/DockerizedApplication.java
@@ -1,12 +1,18 @@
 package com.alxayeed.dockerized;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class DockerizedApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DockerizedApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Application started...");
 		SpringApplication.run(DockerizedApplication.class, args);
 	}
 
